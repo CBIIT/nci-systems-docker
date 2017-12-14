01725d54ac95b71e1f80ad3c58c01db8f09a5934
gadilasr <gadilasr@nih.gov>
2017-12-14 14:10:52 -0500

    handling both cbiit and cbiit-dev user accounts login

--- bash-scripts/dockerbuild.sh
+++ bash-scripts/dockerbuild.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 #title           :dockerbuild.sh
 #description     :This script builds docker images on regular basis based on crond definition.
 #author		 	 :NCI CBIIT Apphosting Team
@@ -8,9 +6,10 @@
 #usage		     :sh dockerbuild.sh  Note this script assumes the values of DOCKER_USER and DOCKER_USER_PW are env variables( export DOCKER_USER=user, export DOCKER_USER_PW=password)
 
 
-SVNURL=https://github.com/CBIIT/nci-systems-docker-pub/trunk/build
-WORKDIR=~/docker-autobuild
-DOCKERFILE_BASE=$WORKDIR/build
+SVNURL=https://github.com/CBIIT/nci-systems-docker-pub/trunk/$Location/$Image
+
+WORKDIR=${WORKSPACE}/docker-autobuild
+DOCKERFILE_BASE=$WORKDIR/$Location
 
 #Ensure the workdir exist if not
 if [[ ! -d $WORKDIR ]];
@@ -28,18 +27,37 @@ fi
  cd $WORKDIR
  svn checkout $SVNURL
 
+#echo "Development user $DOCKER_DEV_USER"
+#echo "Production user $DOCKER_PROD_USER"
 #Login to ncidockerhub.nci.nih.gov
-docker login ncidockerhub.nci.nih.gov --username $DOCKER_USER --password $DOCKER_USER_PW
+if [ "$Location" = "cbiit-dev" ]; then
+  echo "login to ncidockerhub with $DOCKER_DEV_USER"
+  docker login ncidockerhub.nci.nih.gov --username $DOCKER_DEV_USER --password $DOCKER_USER_PW
+fi
 
-#List all the docker images directory in the build folder
-IMAGES=`find build -type d -maxdepth 1 | cut -d'/' -f2  | grep -v 'build$' | grep -v '\.svn$'`
+if [ "$Location" = "cbiit" ]; then
+  echo "login to ncidockerhub with $DOCKER_PROD_USER"
+  docker login ncidockerhub.nci.nih.gov --username $DOCKER_PROD_USER --password $DOCKER_USER_PW
+fi
+
+
+#set name for the images
+
+name=$Location
+
+#if [[ $Location == "build" ]];
+#		then
+#		name="cbiit"
+#fi
+
 
 #Build each image and push them to ncidockerhub. 
-for image in $IMAGES
-do
-	pushd build/$image
-	docker build -t ncidockerhub.nci.nih.gov/cbiit/cbiit-$image --no-cache .
-	docker push ncidockerhub.nci.nih.gov/cbiit/cbiit-$image
-	popd
-done
+
+pushd $Image
+docker build -t ncidockerhub.nci.nih.gov/$name/$Image --no-cache .
+docker push ncidockerhub.nci.nih.gov/$name/$Image
+popd
+    
+#Delete all built images
+#docker rmi $(docker images -q) -f
 
