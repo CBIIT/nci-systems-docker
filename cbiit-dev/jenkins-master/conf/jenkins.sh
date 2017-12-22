4361e4fc1b9d58a195cd26be1b4093b0385173e9
NCI CBIIT GitHub Management Account <nciwebscm@users.noreply.github.com>
2017-12-22 11:20:23 -0500

    Update jenkins.sh

--- cbiit-dev/jenkins-master/conf/jenkins.sh
+++ cbiit-dev/jenkins-master/conf/jenkins.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-: "${JENKINS_HOME:="/var/jenkins_home"}"
+: "${JENKINS_HOME:="/local/content/jenkins"}"
 touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
 find /usr/share/jenkins/ref/ -type f -exec bash -c '. /usr/local/bin/jenkins-support; for arg; do copy_reference_file "$arg"; done' _ {} +
